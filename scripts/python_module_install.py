9c1ed57e8029a93ad39f1af771395e2bc7a873ca
U-CHINA\m00622175 <m00622175@DESKTOP-HN1O1R1.china.huawei.com>
2023-12-19 17:28:56 +0800

    v0.1

--- scripts/python_module_install.py
+++ scripts/python_module_install.py
@@ -1,10 +1,18 @@
+import os
+import shutil
+
 import launch
+import re
+import subprocess
+import importlib
 import gradio as gr
 from modules import paths, shared, script_callbacks, scripts, images
 
-
 tsinghua_url = 'https://pypi.tuna.tsinghua.edu.cn/simple/'
-install_path = '/mnt/auto/sd/python'
+default_install_path = '/mnt/auto/sd/python'
+tmpdir = "/tmp"
+
+
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as module_installer:
         create_ui()
@@ -13,48 +21,130 @@ def on_ui_tabs():
 
 
 def create_ui():
-
-    with gr.Column():
-        with gr.Row(scale=1):
+    with gr.Tab(label="Install By Name"):
+        with gr.Column():
+            checkbox = gr.CheckboxGroup(["Input Version", "Install Path", "Index URL"], show_label=False)
             name_box = gr.Textbox(label="Module Name", placeholder="module name to install")
-        with gr.Row(scale=1):
-            version_box = gr.Textbox(label="Module Version", placeholder="module version to install")
-        with gr.Row(scale=1):
+            version_box = gr.Textbox(label="Module Version", placeholder="module version to install", visible=False)
+            install_path = gr.Textbox(label="Install Path", visible=False, interactive=True,
+                                      placeholder="path to install, default path is /mnt/auto/sd/python")
+            index_url = gr.Textbox(label="Index URL", visible=False, interactive=True,
+                                   placeholder="Index URL, default: https://pypi.tuna.tsinghua.edu.cn/simple/")
             install_button = gr.Button(value="Install", variant='primary')
+
+    with gr.Tab(label="Install By Requirements"):
+        checkbox2 = gr.CheckboxGroup(["Install Path", "Index URL"], show_label=False)
+        file_upload = gr.File()
+        install_path2 = gr.Textbox(label="Install Path", visible=False, interactive=True,
+                                   placeholder="path to install, default path is /mnt/auto/sd/python")
+        index_url2 = gr.Textbox(label="Index URL", visible=False, interactive=True,
+                                placeholder="Index URL, default: https://pypi.tuna.tsinghua.edu.cn/simple/")
+        upload_button = gr.UploadButton("Select Requirements File", file_types=["text"], file_count="single")
+        upload_button.upload(fn=upload_file, inputs=upload_button, outputs=file_upload)
+        install_requirement = gr.Button(label="Install", variant="primary")
 
     with gr.Row():
         with gr.Column(scale=10):
-            warning_box = gr.HTML("", elem_id=f"module_installer_warning_box", elem_classes="moduleInstallerWarning")
+            warning_box = gr.HTML("install result", elem_id=f"module_installer_warning_box",
+                                  elem_classes="moduleInstallerWarning")
+
+    def checkbox_handler(input_str):
+        a = version_box.update(visible=True) if "Input Version" in input_str else version_box.update(visible=False)
+        b = install_path.update(visible=True) if "Install Path" in input_str else install_path.update(visible=False)
+        c = index_url.update(visible=True) if "Index URL" in input_str else index_url.update(visible=False)
+        return a, b, c
+
+    def checkbox2_handler(input_str):
+        a = install_path2.update(visible=True) if "Install Path" in input_str else install_path2.update(visible=False)
+        b = index_url2.update(visible=True) if "Index URL" in input_str else index_url2.update(visible=False)
+        return a, b
 
     install_button.click(
         fn=module_install,
-        inputs=[name_box, version_box],
+        inputs=[name_box, version_box, install_path, index_url],
+        outputs=[warning_box],
+        show_progress=True
+    )
+
+    checkbox.change(
+        fn=checkbox_handler,
+        inputs=[checkbox],
+        outputs=[version_box, install_path, index_url]
+    )
+    checkbox2.change(
+        fn=checkbox2_handler,
+        inputs=[checkbox2],
+        outputs=[install_path2, index_url2]
+    )
+
+    install_requirement.click(
+        fn=module_install_byfile,
+        inputs=[file_upload, install_path, index_url],
         outputs=[warning_box],
         show_progress=True
     )
 
 
-def module_install(name_box, version_box):
+def module_install(name_box, version_box, install_path, index_url):
     result = "<div style='color:#999; font-size:10px' align='center'>"
     if name_box == "":
         result += f"please input module name you would like to install</div>"
         return result
 
-    module_name = name_box if version_box == "" else name_box + "==" + version_box
-    install_module(name_box, version_box)
-    result += f"{module_name} install success</div>"
+    result += install_module(name_box, version_box, install_path, index_url)
     return result
 
 
-def install_module(module_name, version_box):
-    if not launch.is_installed(module_name):
-        module = module_name if version_box == "" else module_name + "==" + version_box
-        index_url = f'--index-url {launch.index_url}' if launch.index_url != '' else f'--index-url {tsinghua_url}'
-        launch.run(f'"{launch.python}" -m pip install {module} -t {install_path} --prefer-binary {index_url}',
-                   desc=f"Installing {module}",
-                   errdesc=f"Couldn't install {module}")
+def install_module(module_name, version_box, install_path, index_url):
+    module = module_name if version_box == "" else module_name + "==" + version_box
+    index_url = launch.index_url if launch.index_url != '' else (index_url if index_url != "" else tsinghua_url)
+    install_path = install_path if install_path != "" else default_install_path
 
-    return True
+    if not launch.is_installed(module_name):
+        cmd = [launch.python, "-m", "pip", "install", module, "-t", install_path, "--prefer-binary", "--index-url",
+               index_url]
+        try:
+            subprocess.run(cmd, check=True)
+            print(f"Installing {module}")
+            # 尝试导入模块来检查是否安装成功
+            importlib.import_module(module_name)
+            print(f"{module} installed successfully")
+            return f"{module} install success</div>"
+        except subprocess.CalledProcessError:
+            print(f"Couldn't install {module}")
+            return f"Couldn't install {module}</div>"
+        except ImportError:
+            print(f"{module} not found after installation")
+            return f"{module} not found after installation</div>"
+
+    return f"{module} install success</div>"
+
+
+def module_install_byfile(file, install_path, index_url):
+    file_name = os.path.basename(file.name)
+    index_url = launch.index_url if launch.index_url != '' else (index_url if index_url != "" else tsinghua_url)
+    install_path = install_path if install_path != "" else default_install_path
+    cmd = [launch.python, "-m", "pip", "install", "-r", tmpdir+"/"+file_name, "-t", install_path, "--prefer-binary",
+           "--index-url", index_url]
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"{file_name} installed successfully")
+        return f"{file_name} install success</div>"
+    except subprocess.CalledProcessError:
+        print(f"Couldn't install {file_name}")
+        return f"Couldn't install {file_name}</div>"
+
+
+def upload_file(file_obj):
+    shutil.copy(file_obj.name, tmpdir)
+    return file_obj.name
+
+
+def is_valid_module_name(module_name):
+    """Check if a module name is valid according to Python's naming rules."""
+    # The regular expression pattern for a valid Python identifier
+    pattern = r'^[a-zA-Z_][a-zA-Z0-9_]*$'
+    return bool(re.match(pattern, module_name))
 
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
